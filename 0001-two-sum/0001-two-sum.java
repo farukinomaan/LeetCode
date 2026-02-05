8c98ff3e7a1c4f5ba62f8a27fbf4500168758775
Nomaan Faruki <99551458+farukinomaan@users.noreply.github.com>
2026-02-05 18:22:50 +0530

    Time: 2 ms (99.15%), Space: 46.7 MB (85.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,7 @@
+// TOP 50 OA 
+// Nomaan Faruki
+// Optimal soln
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap <Integer, Integer> map = new HashMap();
@@ -16,3 +20,19 @@ class Solution {
         
     }
 }
+
+// Brute Soln
+// class Solution{
+//     public int [] twoSum(int[] nums, int target){
+//         for(int i=0;i<numslength;i++){
+//             for (int j=i+1;j<nums.length;j++){
+//                 if(nums[i]+nums[j]== target){
+//                     return new int [] {i,j};
+//                 }
+//             }
+//         }
+
+//         return new int [] {};
+
+//     } 
+// }
