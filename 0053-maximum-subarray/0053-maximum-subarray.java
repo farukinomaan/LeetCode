b79d7fbf4cbbf11412ca9b79926b8aba956f86cc
Nomaan Faruki <99551458+farukinomaan@users.noreply.github.com>
2026-02-08 23:41:09 +0530

    Time: 1 ms (99.6%), Space: 77.4 MB (44.67%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max=0;
+        int newmax= nums[0];
+        for(int i=0;i<nums.length;i++){ 
+           max+=nums[i];
+           if(max>newmax){
+            newmax=max;
+           }
+
+           if(max<0){
+            max=0;
+           }
+        }
+        return newmax;
+    }
+}
