a47924edfb030b881b3c6a7ba59e7d5c4856a375
Nomaan Faruki <99551458+farukinomaan@users.noreply.github.com>
2026-02-10 23:52:11 +0530

    Time: 5 ms (56.18%), Space: 46.8 MB (40.38%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
+        }
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            rows.add(new StringBuilder());
+        }
+
+        int currRow = 0;
+        boolean goingDown = false; 
+
+        for (char c : s.toCharArray()) {
+            rows.get(currRow).append(c);
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            if (goingDown) {
+                currRow++;
+            } else {
+                currRow--;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
