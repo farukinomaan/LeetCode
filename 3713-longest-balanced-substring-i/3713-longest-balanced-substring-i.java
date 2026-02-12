898938a882dd09f08ec90a5cc5ca1332aa2d8c9a
Nomaan Faruki <99551458+farukinomaan@users.noreply.github.com>
2026-02-12 23:17:12 +0530

    Time: 34 ms (94.19%), Space: 47.2 MB (23.97%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestBalanced(String s) {
+        int n = s.length();
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            int[] count = new int[26];
+            int distinct = 0;
+            int maxFreq = 0;
+            
+            for (int j = i; j < n; j++) {
+                int charIdx = s.charAt(j) - 'a';
+                if (count[charIdx] == 0) {
+                    distinct++;
+                }
+                count[charIdx]++;
+                maxFreq = Math.max(maxFreq, count[charIdx]);
+                
+                if (distinct * maxFreq == (j - i + 1)) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+}
