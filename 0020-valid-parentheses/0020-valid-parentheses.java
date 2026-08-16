36f818bdd32344cc917f5fdd0d4726041ca26d2c
Nomaan Faruki <99551458+farukinomaan@users.noreply.github.com>
2026-08-16 23:48:12 +0530

    Time: 3 ms (86.08%), Space: 43.2 MB (73.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,20 @@
-// Top 50 OA
-// Nomaan Faruki
 class Solution {
     public boolean isValid(String s) {
-        Stack <Character> stack = new Stack<>();
+        
+        Stack<Character> stack= new Stack<>();
 
-        for(int i=0; i<s.length();i++){
-            char c= s.charAt(i);
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
 
             if(c=='('){
                 stack.push(')');
-            }
-            else if(c == '{'){
+            }else if(c== '['){
+                stack.push(']');
+            }else if(c=='{'){
                 stack.push('}');
             }
-            else if(c== '['){
-                stack.push(']');
-            }
-            else if(stack.empty() || stack.pop() != c){
+
+            else if (stack.isEmpty() || stack.pop() !=c){
                 return false;
             }
         }
